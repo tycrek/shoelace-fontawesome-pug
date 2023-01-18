c1efe396bec5dc33b3378613cc2a900e45c19898
tycrek <gpg+github@jmoore.dev>
2023-01-17 19:17:33 -0700

    fix: shouldn't be assigning here

--- code.js
+++ code.js
@@ -4,7 +4,7 @@ registerIconLibrary('fa', {
 	resolver: (name) => {
 		const icon = name.replace(/^fa([rsltdb]|(ss))-/, '');
 		const sub = name.substring(0, 4);
-		const dir = (sub === 'fas-') ? dir = 'solid' : (sub === 'fal-') ? dir = 'light' : (sub === 'fat-') ? dir = 'thin' : (sub === 'fad-') ? dir = 'duotone' : (sub === 'fab-') ? dir = 'brands' : (name.substring(0, 5) === 'fass-') ? dir = 'sharp-solid' : 'regular';
+		const dir = (sub === 'fas-') ? 'solid' : (sub === 'fal-') ? 'light' : (sub === 'fat-') ? 'thin' : (sub === 'fad-') ? 'duotone' : (sub === 'fab-') ? 'brands' : (name.substring(0, 5) === 'fass-') ? 'sharp-solid' : 'regular';
 		return `%FONT_AWESOME_CDN%/svgs/${dir}/${icon}.svg`;
 	},
 	mutator: svg => svg.setAttribute('fill', 'currentColor')
